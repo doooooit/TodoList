da48011d6df9231b068d64d573b04ce9e3989751
shangke <shangke01@gmail.com>
2017-03-19 17:49:56 +0800

    V0.0.3 [新增] 新建事项时弹出层 [优化] UI基本定型

--- js/todolist.js
+++ js/todolist.js
@@ -1,6 +1,92 @@
 function registry() {
     var reg = document.createElement('div');
+    reg.id = "registry-container";
 
+    var nodes = '<div id="beautifier"></div>' +
+        '<form id="registry">' +
+            '<input id="topic" type="text" value="事项主题" name="topic" required="required" max="60" />' +
+            '<div id="expiration-date">' +
+                '<label id="date-description">有效日期截止：</label>' +
+                '<select id="select-date-year">' +
+                    '<option value=""></option>' +
+                '</select>' +
+                '<label id="year">年</label>' +
+                '<select id="select-date-month">' +
+                    '<option value="1" selected="selected">1</option>' +
+                    '<option value="2">2</option>' +
+                    '<option value="3">3</option>' +
+                    '<option value="4">4</option>' +
+                    '<option value="5">5</option>' +
+                    '<option value="6">6</option>' +
+                    '<option value="7">7</option>' +
+                    '<option value="8">8</option>' +
+                    '<option value="9">9</option>' +
+                    '<option value="10">10</option>' +
+                    '<option value="11">11</option>' +
+                    '<option value="12">12</option>' +
+                '</select>' +
+                '<label id="month">月</label>' +
+                '<select id="select-date-day">' +
+                    '<option value="1" selected="selected">1</option>' +
+                    '<option value="2">2</option>' +
+                    '<option value="3">3</option>' +
+                    '<option value="4">4</option>' +
+                    '<option value="5">5</option>' +
+                    '<option value="6">6</option>' +
+                    '<option value="7">7</option>' +
+                    '<option value="8">8</option>' +
+                    '<option value="9">9</option>' +
+                    '<option value="10">10</option>' +
+                    '<option value="11">11</option>' +
+                    '<option value="12">12</option>' +
+                    '<option value="13">13</option>' +
+                    '<option value="14">14</option>' +
+                    '<option value="15">15</option>' +
+                    '<option value="16">16</option>' +
+                    '<option value="17">17</option>' +
+                    '<option value="18">18</option>' +
+                    '<option value="19">19</option>' +
+                    '<option value="20">20</option>' +
+                    '<option value="21">21</option>' +
+                    '<option value="22">22</option>' +
+                    '<option value="23">23</option>' +
+                    '<option value="24">24</option>' +
+                    '<option value="25">25</option>' +
+                    '<option value="26">26</option>' +
+                    '<option value="27">27</option>' +
+                    '<option value="28">28</option>' +
+                '</select>' +
+                '<label id="day">日</label>' +
+            '</div>' +
+            '<textarea id="content">事项内容</textarea>' +
+            '<div id="registry-buttons">' +
+                '<button class="button" id="cancel">取消</button>' +
+                '<input class="button" id="submit" type="submit" value="提交" name="submitBtn" />' +
+            '</div>' +
+        '</form>'
+
+    reg.innerHTML = nodes;
+    document.body.appendChild(reg);
+    var h = window.innerHeight;
+    var w = window.innerWidth;
+    reg.style.top = (h - reg.offsetHeight) / 2 - 30 + "px";
+    reg.style.left = (w - reg.offsetWidth) / 2 + "px";
+
+    var topic = document.querySelector('#topic');
+    topic.onclick = function () {
+        topic.value = '';
+    }
+
+    var content = document.querySelector('#content');
+    content.onclick = function () {
+        content.innerText = '';
+    }
+
+    var cancel = document.querySelector('#cancel');
+    cancel.onclick = function () {
+        document.body.removeChild(document.querySelector('#registry-container'));
+        document.body.removeChild(document.querySelector('#mask'));
+    }
 }
 
 function createNew() {
@@ -15,7 +101,10 @@ function createNew() {
     mask.style.width = w + "px";
     // 将 mask 元素插入文档中
     document.body.appendChild(mask);
+    registry();
+
     mask.onclick = function () {
+        document.body.removeChild(document.querySelector('#registry-container'));
         document.body.removeChild(mask);
     }
 }
